f3d32452c54122869539c3723598da25a497b1ab
Dan <98132259+Dan090807@users.noreply.github.com>
2022-10-03 17:33:42 -0600

    Added classes Lizard, Bush

--- src/Model/Stats.java
+++ src/Model/Stats.java
@@ -14,6 +14,31 @@ public abstract class Stats {
 	protected int defense;
 	protected int speed;
 	
+	public int getHp() {
+		return hp;
+	}
+	public void setHp(int hp) {
+		this.hp = hp;
+	}
+	public int getAttack() {
+		return attack;
+	}
+	public void setAttack(int attack) {
+		this.attack = attack;
+	}
+	public int getDefense() {
+		return defense;
+	}
+	public void setDefense(int defense) {
+		this.defense = defense;
+	}
+	public int getSpeed() {
+		return speed;
+	}
+	public void setSpeed(int speed) {
+		this.speed = speed;
+	}
+	
 	
 
 }
--- src/Model/Warrior.java
+++ src/Model/Warrior.java
@@ -7,6 +7,51 @@ package Model;
  * @author Dan
  *
  */
-public class Warrior {
+public class Warrior extends Stats {
+	
+	private String name;
+	private String victory;
+	private String start;
+	private String defeat;
+	
+	public Warrior(String Name, String Victory, String Start, String Defeat){
+		setName(name);
+		setVictory(victory);
+		setDefeat(defeat);
+		setStart(start);
+	}
 
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getVictory() {
+		return victory;
+	}
+
+	public void setVictory(String victory) {
+		this.victory = victory;
+	}
+
+	public String getStart() {
+		return start;
+	}
+
+	public void setStart(String start) {
+		this.start = start;
+	}
+
+	public String getDefeat() {
+		return defeat;
+	}
+
+	public void setDefeat(String defeat) {
+		this.defeat = defeat;
+	}
+	
+	
 }
